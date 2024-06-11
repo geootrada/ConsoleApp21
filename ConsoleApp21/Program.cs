b05fe54bf9ebe26f53577770db72ea415a9c5011
geootrada <geootrada@yandex.ru>
2024-06-11 18:50:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startRandomRange = 1;
+            int endRandomRange = 100;
+            int baseNumber = 2;
+            int exponent = 2;
+            int minExponent = 1;
+            int resultingNumber = 0;
+
+            Random random = new Random();
+            int setNumber = random.Next(startRandomRange, endRandomRange);
+
+            for (int i = baseNumber; i <= setNumber; i *= exponent)
+            {
+                minExponent++;
+                resultingNumber = i * exponent;
+            }
+
+            Console.WriteLine($"Сгенерированное чмсло {setNumber}\nМинимальная степени двойки, согласно задания {minExponent}\nПолученное число {resultingNumber}");
+            Console.ReadKey();
+        }
+    }
+}
